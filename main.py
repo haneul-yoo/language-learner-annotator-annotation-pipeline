c1dbcd9275f9b277fa208a9341b1a8d9ddbd0426
haneul-yoo <skysky2387@gmail.com>
2022-02-16 16:23:43 +0900

    timeout 30>240

--- main.py
+++ main.py
@@ -312,4 +312,11 @@ def task_done():
 
 init_paths()
 if __name__ == '__main__':
+    # gunicorn_error_logger = logging.getLogger('gunicorn.error')
+    # app.logger.handlers.extend(gunicorn_error_logger.handlers)
+    # app.logger.setLevel(logging.DEBUG)
+    # app.logger.debug('this will show in the log')
+    app.logger.addHandler(logging.StreamHandler(sys.stdout))
+    app.logger.setLevel(logging.DEBUG)
+
     app.run(debug=True)
--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 # gunicorn -w 8 -b 0.0.0.0:58108 main:app
 # gunicorn -w 8 -b 0.0.0.0:5000 main:app
-gunicorn -w 8 -b 0.0.0.0:5000 -t 240 main:app
+gunicorn main:app \
+    --workers 8 \
+    --bind 0.0.0.0:5000 \
+    --access-logfile
