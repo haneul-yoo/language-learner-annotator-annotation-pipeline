208d86954e642a9ba3448d62cf22fc2e7b51155b
haneul-yoo <skysky2387@gmail.com>
2022-02-17 20:05:03 +0900

    not connect to NAS

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # gunicorn -w 8 -b 0.0.0.0:58108 main:app
 # gunicorn -w 8 -b 0.0.0.0:5000 main:app
 gunicorn main:app \
-    --workers 8 \
+    --workers 128 \
     --bind 0.0.0.0:5000 \
     --access-logfile - \
     --error-logfile - \
